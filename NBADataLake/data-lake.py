f18e1e70a301210527f865979b4b10929c77c21d
Kimutaieddy <eddykimutai@outlook.com>
2025-01-17 17:27:51 +0300

    Add AWS configuration variables to data-lake.py

--- NBADataLake/data-lake.py
+++ NBADataLake/data-lake.py
@@ -5,3 +5,10 @@ import requests
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
+# AWS configuration
+region ="us-east-1"
+bucket_name = "Marara_nba-data-lake"
+glue_db_name = "Marara_nba_data_lake"
+athena_output_location = "s3://Marara_nba-data-lake/athena_output"
