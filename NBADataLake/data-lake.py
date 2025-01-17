dbd9139db3aeafebad3d935f34fc37a745c8b507
Kimutaieddy <eddykimutai@outlook.com>
2025-01-17 17:36:59 +0300

    Add AWS client initialization to data-lake.py

--- NBADataLake/data-lake.py
+++ NBADataLake/data-lake.py
@@ -16,3 +16,9 @@ athena_output_location = "s3://Marara_nba-data-lake/athena_output"
 # Sportsdata.io configurations (loaded from .env)
 api_key = os.getenv("SPORTSDATAIO_API_KEY")
 nba_endpoint = os.getenv("SPORTSDATAIO_NBA_ENDPOINT")
+
+# Create AWS clients
+s3_Client = boto3.client('s3', region_name=region)
+glue_Client = boto3.client('glue', region_name=region)
+athena_Client = boto3.client('athena', region_name=region) 
+
