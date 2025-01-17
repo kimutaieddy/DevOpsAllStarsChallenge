8b5cd1f18396c9d1f05c4b18a1026cf797ea0eba
Kimutaieddy <eddykimutai@outlook.com>
2025-01-17 17:31:49 +0300

    add sport-io configs

--- NBADataLake/data-lake.py
+++ NBADataLake/data-lake.py
@@ -12,3 +12,5 @@ region ="us-east-1"
 bucket_name = "Marara_nba-data-lake"
 glue_db_name = "Marara_nba_data_lake"
 athena_output_location = "s3://Marara_nba-data-lake/athena_output"
+
+# Sportsdata.io configurations (loaded from .env)
